270d0db4cad21ac1058f017a2cdaecd780f038d9
Alexandr.Smirnov <sliber@yandex.ru>
2021-11-12 12:01:11 +0300

    Whats up

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-# display command line options
 
-count=1
-for param in "$@"; do
-    echo "Next parameter: $param"
-    count=$(( $count + 1 ))
-done
 
-echo "====="
+    echo "\$@ Parameter #$count = $param"
